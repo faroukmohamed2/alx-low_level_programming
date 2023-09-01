2e8c01cf829471820b53cbd7e457bf6457fcc620
farouk <farmo6995@gmail.com>
2023-09-01 05:24:27 -0700

    readme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,9 @@
  * main - prints the name of the program
  * @argc: the number of argumens
  * @argv: the arguments
+ * Return: 0 
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
