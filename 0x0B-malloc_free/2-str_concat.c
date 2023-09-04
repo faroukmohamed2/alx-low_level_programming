f2db3defd1599cfdd827d88bd79aa7e09ee61375
farouk <farmo6995@gmail.com>
2023-09-04 08:45:47 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+		return ('\0');
 	if (s1 != NULL || s2 == NULL)
 	{
 	for (i = 0; *(s1 + i) != '\0';)
