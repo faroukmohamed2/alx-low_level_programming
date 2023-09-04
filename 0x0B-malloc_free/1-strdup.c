4195c8fa775cbec605d726839dc347f5d689a2b6
farouk <farmo6995@gmail.com>
2023-09-04 08:03:22 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+#include <<stdlib.h>
+/**
+ * _strdup - duplicate a string
+ * @str: the strinng
+ * Return: the str
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i;
+	
+	ptr = (char *)malloc(sizeof(str));
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+	return (ptr);
+}
