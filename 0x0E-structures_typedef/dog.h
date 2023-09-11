e3d5acda697d759048b18b1c2dcf51d210c7604a
farouk <farmo6995@gmail.com>
2023-09-11 12:08:20 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - declare variables
+ * @name: the name
+ * @ae: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
