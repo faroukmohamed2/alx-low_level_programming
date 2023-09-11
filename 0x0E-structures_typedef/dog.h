93f009bcd805c60fbdda2c2babbd53017f6066cc
farouk <farmo6995@gmail.com>
2023-09-11 12:23:52 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _DOG_H
 #define _DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog - declare variables
  * @name: the name
@@ -13,4 +12,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
