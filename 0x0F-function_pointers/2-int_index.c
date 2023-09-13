abfbc3cf4e564b811e1dad80ff64404c03f7c1b9
farouk <farmo6995@gmail.com>
2023-09-13 09:25:20 -0700

    search_integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,23 +8,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	if (size <= 0)
-		return (-1);
 	if (array && cmp)
 	{
+		if (size <= 0)
+			return (-1)
 		for (; i < size; i++)
 		{
-			if (cmp(array[i]) == 0)
+			if (cmp(array[i]))
 			{
-				j++;
-			}
-			else
 				return (i);
+			}
 		}
 	}
-		if (j == size)
-			return (-1);
 		return (i);
 }
