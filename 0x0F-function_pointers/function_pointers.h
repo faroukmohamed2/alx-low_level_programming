e544c7cd28db1fbcbe7b6964289c42134076036a
farouk <farmo6995@gmail.com>
2023-09-13 08:44:21 -0700

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef __FUNCTION__H
 #define __FUNCTION__H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
