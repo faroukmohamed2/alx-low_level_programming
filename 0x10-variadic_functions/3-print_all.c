9ab0ee20e9f9d8031ab77c523b27e14720b40d26
farouk <farmo6995@gmail.com>
2023-09-14 10:18:22 -0700

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,9 +40,11 @@ void print_all(const char * const format, ...)
 		case 's' :
 			str = va_arg(list, char *);
 			if (!str)
+			{
 				printf("(nil)");
-			else
-				printf("%s", str);
+				break;
+			}
+			printf("%s", str);
 			k = 1;
 			break;
 		}
