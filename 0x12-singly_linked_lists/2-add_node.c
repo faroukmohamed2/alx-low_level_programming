72d1021a5462f236277476c70f48d57216fb4873
farouk <farmo6995@gmail.com>
2023-09-21 15:02:31 -0700

    lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node: add a node to the list
+ * @head: the head
+ * @str: a string to be dublicated
+ * Return: addres of the head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (; str[i];)
+		i++;
+	new->len = i;
+	new->next = *head;
+	return (new);
+}
