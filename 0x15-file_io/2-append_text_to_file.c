358624ab815930c2d35c3fd157e616f83485d323
farouk <farmo6995@gmail.com>
2023-10-02 13:05:59 -0700

    create_append_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends atext at the end of a file
+ * @filename: the filename
+ * @text_content: the text to be appended
+ * Return: 1 on success -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i = 0, szwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	for (; text_content[i]; i++)
+		;
+	if (text_content)
+	{
+		szwr = write(fd, text_content, i);
+		if (szwr == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
