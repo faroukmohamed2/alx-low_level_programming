821b0bd77aa37665fc8fa8ae13b61c73fa083cf6
farouk <farmo6995@gmail.com>
2023-10-27 15:02:41 -0700

    bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ unsigned int _pow(unsigned int x, int y);
 void print_binary(unsigned long int n);
 void _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
