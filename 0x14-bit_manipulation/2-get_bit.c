814c2a3476f346a535293e0a23ad2444d7017c4f
farouk <farmo6995@gmail.com>
2023-10-27 14:36:15 -0700

    bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_bit - get the bit of the index
+ * @n: the number
+ * @index: the index
+ * Return: the index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int x = n;
+	int i = 0, sizz = 0;
+	int *num;
+
+	while (n > 0)
+	{
+		 n = n / 2;
+		 sizz++;
+	}
+	num = malloc(sizeof(int) * (sizz + 1));
+	for (i = 0; i < sizz; i++)
+	{
+		*(num + i) = x % 2;
+		x = x / 2;
+	}
+	return (num[index]);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ unsigned int binary_to_uint(const char *b);
 unsigned int _pow(unsigned int x, int y);
 void print_binary(unsigned long int n);
 void _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
