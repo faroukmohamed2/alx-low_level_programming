ec7e6e872c3bc32d878af173812fa8624f99da86
farouk <farmo6995@gmail.com>
2023-11-01 11:51:03 -0700

    file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * create_file - creates  a file
+ * @filename: the filename to be created
+ * @text_content: a string to be written in the file
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
