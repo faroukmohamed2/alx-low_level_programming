e035893d30f179a4889ad38e4e32dd6504284998
farouk <farmo6995@gmail.com>
2023-11-01 10:08:17 -0700

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <fcntl.h>
 /**
  * read_textfile - reads a file and prints it
  * @filename: the file name
@@ -7,16 +8,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
+	int fd;
+	ssize_t rdsz, wrsz;
 	if (!filename)
 		return (0);
-	char *buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
-	int fd = open(filename,  O_RDONLY);
+	fd = open(filename,  O_RDONLY);
 	if (fd < 0)
 		return (0);
-	int rdsz = read(fd, buf, letters);
-	int wrsz = write(_STDOUT_FILENO, buf, rdsz);
+	rdsz = read(fd, buf, letters);
+	wrsz = write(STDOUT_FILENO, buf, rdsz);
 	close(fd);
 	free(buf);
 	return (wrsz);
