e218b1717af5565d92d0286d40483b1ae07e4c96
farouk <farmo6995@gmail.com>
2023-11-02 14:53:43 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t rdsz;
 	int i = 0;
 
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
 	for (; text_content[i]; i++)
 		;
-	fd = open(filename, O_CREAT | O_RDONLY | O_WRONLY);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd < 0)
 		return (-1);
-	rdsz = read(fd, text_content, i);
-	write(O_WRONLY, text_content, rdsz);
+	write(fd, text_content, i);
 	close(fd);
 	return (1);
 }
